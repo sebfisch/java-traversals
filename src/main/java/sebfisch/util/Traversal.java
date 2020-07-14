07aa5d603c1be27553cf6da3817a13320ff5f459
Sebastian Fischer <mail@sebfisch.de>
2020-07-14 08:02:12 +0000

    added maven site configuration with checkstyle report

--- src/main/java/sebfisch/util/Traversal.java
+++ src/main/java/sebfisch/util/Traversal.java
@@ -4,8 +4,8 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
-interface Traversal<S, P> extends Function<Consumer<P>, Consumer<S>> {
-  public class For<T> implements Traversal<T, T> {
+public interface Traversal<S, P> extends Function<Consumer<P>, Consumer<S>> {
+  class For<T> implements Traversal<T, T> {
     @Override
     public Consumer<T> apply(final Consumer<T> ct) {
       return ct;
