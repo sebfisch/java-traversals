e5d566ba3bb14260bccd1ef2e4c5ebce32bc5103
Sebastian Fischer <mail@sebfisch.de>
2020-07-15 11:18:38 +0000

    more reporting and some refactoring

--- src/main/java/sebfisch/util/Traversal.java
+++ src/main/java/sebfisch/util/Traversal.java
@@ -4,21 +4,45 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
-public interface Traversal<S, P> extends Function<Consumer<P>, Consumer<S>> {
-  class For<T> implements Traversal<T, T> {
+/**
+ * A Traversal can be used to traverse parts of structured data.
+ *
+ * @param <S> type of a data structure
+ * @param <P> type of traversed parts
+ */
+public abstract class Traversal<S, P> implements Function<Consumer<P>, Consumer<S>> {
+
+  /**
+   * Creates a traversal for a specified type that traverses the root of given data.
+   *
+   * @param <T> type of traversed data
+   */
+  public static class For<T> extends Traversal<T, T> {
     @Override
     public Consumer<T> apply(final Consumer<T> ct) {
       return ct;
     }
   }
 
-  default Stream<P> traverse(final S s) {
-    final Stream.Builder<P> sb = Stream.builder();
-    traverse(s, sb::add);
-    return sb.build();
+  /**
+   * Provides access to traversed parts with a {@link Consumer}.
+   *
+   * @param s structured data
+   * @param cp consumer of parts
+   */
+  public void traverse(final S s, final Consumer<P> cp) {
+    apply(cp).accept(s);
   }
 
-  default void traverse(final S s, final Consumer<P> cp) {
-    apply(cp).accept(s);
+  /**
+   * Provides access to a {@link Stream} of traversed parts.
+   *
+   * @param s structured data
+   * @return stream of traversed parts
+   */
+  public Stream<P> partsOf(final S s) {
+    final Stream.Builder<P> sb = Stream.builder();
+    traverse(s, sb);
+    return sb.build();
   }
 }
--- src/test/java/sebfisch/util/TraversalTests.java
+++ src/test/java/sebfisch/util/TraversalTests.java
@@ -4,9 +4,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-class TraversalTests {
+/**
+ * This class demonstrates the use of {@link sebfisch.util.Traversal}s by documenting the expected
+ * behavior of their methods.
+ */
+public class TraversalTests {
+
+  /**
+   * Tests that the {@link sebfisch.util.Traversal#partsOf} method returns exactly one element when
+   * applied on a traversal constructed with {@link sebfisch.util.Traversal.For}.
+   */
   @Test
-  public void testStreaming() {
-    assertEquals(42, new Traversal.For<Integer>().traverse(42).findFirst().orElse(0));
+  public void testPartsOfOnForHasSizeOne() {
+    assertEquals(1, new Traversal.For<Integer>().partsOf(42).count());
+  }
+
+  /**
+   * Tests that the {@link sebfisch.util.Traversal#partsOf} method returns a stream containing its
+   * argument when applied on a traversal constructed with {@link sebfisch.util.Traversal.For}.
+   */
+  @Test
+  public void testPartsOfOnForContainsRoot() {
+    assertEquals(42, new Traversal.For<Integer>().partsOf(42).findFirst().orElseThrow());
   }
 }
