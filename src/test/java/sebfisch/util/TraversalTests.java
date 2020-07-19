6cb5455f031b9c4be68db3b37f544c19fe0907ed
Sebastian Fischer <mail@sebfisch.de>
2020-07-19 07:07:58 +0000

    use parameterized tests

--- src/test/java/sebfisch/util/TraversalTests.java
+++ src/test/java/sebfisch/util/TraversalTests.java
@@ -2,7 +2,10 @@ package sebfisch.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.IntStream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullSource;
 
 /**
  * This class demonstrates the use of {@link sebfisch.util.Traversal}s by documenting the expected
@@ -10,21 +13,27 @@ import org.junit.jupiter.api.Test;
  */
 public class TraversalTests {
 
+  private static IntStream smallInt() {
+    return IntStream.range(-3, 3);
+  }
+
   /**
    * Tests that the {@link sebfisch.util.Traversal#partsOf} method returns exactly one element when
    * applied on a traversal constructed with {@link sebfisch.util.Traversal.For}.
    */
-  @Test
-  public void testPartsOfOnForHasSizeOne() {
-    assertEquals(1, new Traversal.For<Integer>().partsOf(42).count());
+  @ParameterizedTest
+  @NullSource
+  public void testRootCountIsOne(final Object root) {
+    assertEquals(1, new Traversal.For<Object>().partsOf(root).count());
   }
 
   /**
    * Tests that the {@link sebfisch.util.Traversal#partsOf} method returns a stream containing its
    * argument when applied on a traversal constructed with {@link sebfisch.util.Traversal.For}.
    */
-  @Test
-  public void testPartsOfOnForContainsRoot() {
-    assertEquals(42, new Traversal.For<Integer>().partsOf(42).findFirst().orElseThrow());
+  @ParameterizedTest
+  @MethodSource("smallInt")
+  public void testRootPartsContainRoot(final int root) {
+    assertEquals(root, new Traversal.For<Integer>().partsOf(root).findFirst().orElseThrow());
   }
 }
